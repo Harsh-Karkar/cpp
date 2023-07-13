da3ecf2362798b1c265e7924d20fa34dbbe5d29a
harsh <harshkarkar333@gmail.com>
2023-07-13 11:16:02 +0530

    13-7-23

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cout<<"hello world\n";
+    cout<<"enter value a: ";
+    cin>>a;
+    cout<<"enter value b:";
+    cin>>b;
+    cout<<"product is : "<<a*b;
+}
